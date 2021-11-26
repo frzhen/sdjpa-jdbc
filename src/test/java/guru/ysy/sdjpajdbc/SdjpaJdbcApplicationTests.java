5d751005db0863c5ad72004aed6c1b4c729b5ab7
Fred Zhen <13911239075@163.com>
2021-11-26 19:15:23 +0800

    Copied Spring Initializer content, modified POM, added banner.txt, copied .gitignore template.

---
+++ src/test/java/guru/ysy/sdjpajdbc/SdjpaJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.ysy.sdjpajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
